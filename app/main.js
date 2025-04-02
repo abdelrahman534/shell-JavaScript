f8e738c00dbd0e15616df04a24035b4529214462
abdelrahman534 <abdelrahmanashraf991@gmail.com>
2025-04-02 18:58:45 +0200

    adding exit and making shell recursive

--- app/main.js
+++ app/main.js
@@ -5,8 +5,19 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// Uncomment this block to pass the first stage
-rl.question("$ ", (answer) => {
-  console.log(`${answer}: command not found\n`);
-  r1.close();
-});
+function prompt(){
+
+      rl.question("$ ", (answer) => {
+        if(answer.trim() === "exit"){
+          console.log("Exiting shell...");
+          rl.close();
+        } 
+        else{
+      console.log(`${answer}: command not found\n`);
+      //recursively call prompt
+      prompt();
+    }}
+  );  
+}
+
+prompt();
